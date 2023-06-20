e6cc622df3ca29d9d0255ac36e9e59bc21bd9d9a
Vlad Epov <vladep622@gmail.com>
2023-06-20 15:30:04 +0300

    formik works

--- frontend/src/cmps/form-user-info.jsx
+++ frontend/src/cmps/form-user-info.jsx
@@ -1,60 +1,61 @@
 import { Formik, Field, Form } from "formik"
 
-export function FormUserInfo() {
+export function FormUserInfo({ handleOnSubmit }) {
 
+    const initialValues = {
+        firstName: "",
+        lastName: "",
+        eMail: "",
+        website: "",
+        linkedIn: "",
+        yearsOfExperience: "",
+        campaignBudget: "",
+    }
     return (
         <Formik
-            initialValues={{ name: "", email: "" }}
-            onSubmit={async (values) => {
-                await new Promise((resolve) => setTimeout(resolve, 500));
-                alert(JSON.stringify(values, null, 2));
-            }}
+            initialValues={initialValues}
+            onSubmit={handleOnSubmit}
         >
             <Form>
-                <Field name="name" type="text" />
-                <Field name="email" type="email" />
-
-
                 <section className="form-user-info">
                     <h2>Fill the following form:</h2>
                     <section>
                         <label>First name:</label>
-                        <input type="text" />
+                        <Field type="text" name="firstName" />
                     </section>
                     <section>
                         <label>Last name:</label>
-                        <input type="text" />
+                        <Field type="text" name="lastName" />
                     </section>
                     <section>
                         <label>Email address:</label>
-                        <input type="text" />
+                        <Field type="email" name="eMail" />
                     </section>
                     <section>
                         <label>Website address:</label>
-                        <input type="text" />
+                        <Field type="text" name="website" />
                     </section>
                     <section>
                         <label>LinkedIn profile address (Optional):</label>
-                        <input type="text" />
+                        <Field type="text" name="linkedIn" />
                     </section>
                     <section>
                         <h4>Answer one of the following questions (optional):</h4>
                         <section>
                             How many years of experience do you
                             have with Facebook Marketing?
-                            <select name="" id="">
+                            <Field as="select" name="yearsOfExperience" id="">
                                 <option value=""></option>
-                                <option value="">no experience</option>
-                                <option value="">0-1 years</option>
-                                <option value="">1-2 years</option>
-                                <option value="">2 or more years</option>
-                                {/* <option value=""></option> */}
-                            </select>
+                                <option value="noExperience">no experience</option>
+                                <option value="0-1">0-1 years</option>
+                                <option value="1-2">1-2 years</option>
+                                <option value="2+">2 or more years</option>
+                            </Field>
                         </section>
                         <section>
                             <label>What was the
                                 biggest campaign budget you have managed in a single month?</label>
-                            <input type="range" min={1000} max={500000} />
+                            <Field type="range" name="campaignBudget" min={1000} max={500000} />
                         </section>
                     </section>
                     <button type="submit">Submit</button>
--- frontend/src/views/add-user-info.jsx
+++ frontend/src/views/add-user-info.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
 import { FormUserInfo } from "../cmps/form-user-info"
 
 
 export function AddUserInfo() {
+    const [form, setForm] = useState(null)
+
+    useEffect(() => {
+        if (form) {
+            // addForm()
+        }
+    }, [form])
+
+    function handleOnSubmit(values) {
+        console.log('values', values)
+        setForm(values)
+    }
 
     return (
-        <FormUserInfo />
+        <FormUserInfo handleOnSubmit={handleOnSubmit} />
     )
 }
